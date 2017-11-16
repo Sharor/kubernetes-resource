5b90d69e03215d9650fecaa11f7ba0b9be9ae303
Simon Grønborg <groenborg27@gmail.com>
2017-11-16 15:43:01 +0100

    replaced stdout with stderr

--- check/check.go
+++ check/check.go
@@ -15,30 +15,31 @@ func main() {
 	if len(os.Args) < 2 {
 	}
 
-	oreq := CheckRequest{}
+	CheckRequest := CheckRequest{}
 
-	err := json.NewDecoder(os.Stdin).Decode(&oreq)
+	err := json.NewDecoder(os.Stdin).Decode(&CheckRequest)
 	if err != nil {
 		panic(err)
 	}
+
 	var stderrBuffer bytes.Buffer
 	var stdOutBuffer bytes.Buffer
-	err = ioutil.WriteFile("cert.crt", []byte(oreq.Source.CertAuthority), 0644)
-	err = ioutil.WriteFile("client.crt", []byte(oreq.Source.ClientCert), 0644)
-	err = ioutil.WriteFile("client.key", []byte(oreq.Source.ClientKey), 0644)
+	err = ioutil.WriteFile("cert.crt", []byte(CheckRequest.Source.CertAuthority), 0644)
+	err = ioutil.WriteFile("client.crt", []byte(CheckRequest.Source.ClientCert), 0644)
+	err = ioutil.WriteFile("client.key", []byte(CheckRequest.Source.ClientKey), 0644)
 
 	cmd := exec.Command("kubectl", "--certificate-authority", "cert.crt", "--client-key",
-		"client.key", "--client-certificate", "client.crt", "--server", oreq.Source.ClusterURL, "get", "pods")
+		"client.key", "--client-certificate", "client.crt", "--server", CheckRequest.Source.ClusterURL, "get", "pods")
 	cmd.Stderr = &stderrBuffer
 	cmd.Stdout = &stdOutBuffer
 	err = cmd.Run()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	fmt.Println(stderrBuffer.String())
-	fmt.Println(stdOutBuffer.String())
+	fmt.Fprintln(os.Stderr, stderrBuffer.String())
+	fmt.Fprintln(os.Stderr, stdOutBuffer.String())
 
 	versions := []Version{}
 	versions = append(versions, Version{
